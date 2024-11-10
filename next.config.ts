87ccba89b24fa35b52136e1b24889f9f67ef05a1
Leandro Favre <atiladevelop@gmail.com>
2024-11-10 02:29:18 -0300

    code removed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/hello-world", // Usa el nombre de tu repositorio
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
