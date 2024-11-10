92b1c3680c5e5104fd71e9e3b5ffaa86e833f12c
Leandro Favre <atiladevelop@gmail.com>
2024-11-10 01:50:19 -0300

    Primer commit - Hola Mundo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/hello-world", // Usa el nombre de tu repositorio
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
